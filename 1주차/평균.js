1bc0ba1cc6a659035e74d9de3750fda70ced7ad4
Han <gksdjf80@naver.com>
2022-04-22 20:04:51 +0900

    [fix]디팩토링

--- 1주차/평균.js
+++ 1주차/평균.js
@@ -1,8 +1,7 @@
-let fs = require('fs');
 let line = require('fs').readFileSync('예제.txt').toString().split('\n');
 let length = parseInt(line[0]);
 let n = line[1].split(' ').map((e) => parseInt(e));
 let max = Math.max(...n);
 let answer = (n.reduce((a, b) => a + b) * 100) / max / length;
 console.log(answer);
-// let line = fs.readFileSync('/dev/stdin').toString().split('\n');
+// let line = require('fs').readFileSync('/dev/stdin').toString().split('\n');
